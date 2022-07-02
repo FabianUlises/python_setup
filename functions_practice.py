ed2413542604fd5025468a84d168addc32fa9b9b
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-02 13:00:07 -0700

    function 3 & print

--- functions_practice.py
+++ functions_practice.py
@@ -4,3 +4,21 @@ def hello():
 #2
 def pack(a, b, c):
     return [a, b, c]
+#3
+def eat_lunch(str):
+  if len(str) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(str)):
+      if i == 0:
+        print(f"First I eat {str[0]}")
+      else:
+        print(f"Next I eat {str[i]}")
+        
+#printing
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
